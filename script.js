3adec74bb306739cb4b79e0ed8c16d664c1cb4fa
안현주 <ahj9680@gmail.com>
2020-08-14 14:30:34 +0900

    pop up

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 Swal.fire({
   title: '나라!',
   text: '설명.',
-  imageUrl: '',
+  imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/1200px-Flag_of_South_Korea.svg.png',
   imageWidth: 400,
   imageHeight: 200,
   imageAlt: 'Custom image',
